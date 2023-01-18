e5fa8ac8a024e55dcfb84a8e0b90cccb3a12c87c
vladosbadmaev <alex.raiven@mail.ru>
2023-01-18 18:54:03 +0300

    Add README.MD

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SalesManager manager = new SalesManager(new int[]{1, 2, 6});
+        System.out.println(manager.max());
     }
 }
